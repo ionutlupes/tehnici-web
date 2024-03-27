bd14e8ef48578b7a6f36d6cc1c0f2aa362b37e31
ionutlupes <162938785+ionutlupes@users.noreply.github.com>
2024-03-27 04:49:51 +0200

    Add files via upload

--- proiect/index.js
+++ proiect/index.js
@@ -1,23 +1,61 @@
 const express = require("express");
-const fs= require('fs');
+const fs = require('fs');
 // const path=require('path');
 // const sharp=require('sharp');
 // const sass=require('sass');
 // const ejs=require('ejs');
 
 
-app= express();
+app = express();
 console.log("Folder proiect", __dirname);
 console.log("Cale fisier", __filename);
 console.log("Director de lucru", process.cwd());
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-app.use("/resurse", express.static(__dirname+"/resurse"));
+app.use("/resurse", express.static(__dirname + "/resurse"));
+// process.cwd()-folderul de unde rulam 
+// dirname-folderul unde se ruleaza codul
+
+app.get("/cerere", function (req, res) {
+    res.send("<b>Hello</b> <span style='color=red>world</span");
+})
+app.get("/data", function (req, res, next) {
+    res.write("data:");
+    next();
+})
+app.get("/data", function (req, res) {
+    res.write("" + new Date());
+    res.end();
+})
 
 
+app.get("/suma/:a/:b", function (req, res) {
+    var suma = parseInt(req.params.a) + parseInt(req.params.b)
+    res.send("" + suma);
+});
 
 
+app.get("/", function (req, res) {
+    // res.sendFile(__dirname + "/index.html")
+    res.render("pagini/index");
+})
+
+app.get(["/", "/index", "/home"], function (req, res) {
+    // res.sendFile(__dirname + "/index.html")
+    res.render("pagini/index");
+})
+
+app.get("/*", function (req, res) {
+    console.log(req.url);
+    res.render("pagini" + req.url, function (err, rezHtml) {
+        console.log("Pagina: ", rezHtml);
+        console.log("Eroare: ", err);
+        res.send(rezHtml)
+
+    })
+});
+// rezHtml continutul paginii html
 
 
 
